41890d75450833427c00e6c7395aa746df44023f
syauqy <syauqilenterano@gmail.com>
2022-12-04 21:10:18 +0700

    add middlewares to routes

--- src/todo/todo.routes.config.ts
+++ src/todo/todo.routes.config.ts
@@ -1,6 +1,8 @@
 import { Application } from "express";
 import { CommonRoutesConfig } from "../common/common.routes.config";
+import { auth } from "../common/middlewares/auth.middleware";
 import todoController from "./controllers/todo.controller";
+import { authz } from "./middlewares/authz.middleware";
 
 export class TodoRoutesConfig extends CommonRoutesConfig {
   private readonly baseURL: string;
@@ -11,10 +13,28 @@ export class TodoRoutesConfig extends CommonRoutesConfig {
   }
 
   configureRoutes() {
-    this.router.post(`${this.baseURL}/todo`, todoController.addTodo)
-    this.router.get(`${this.baseURL}/todo`, todoController.listTodos)
-    this.router.delete(`${this.baseURL}/todo/:todoId`, todoController.deleteTodo)
-    this.router.patch(`${this.baseURL}/todo/:todoId/:status`, todoController.patchStatusTodo)
+    this.router.post(
+      `${this.baseURL}/todo`,
+      auth,
+      todoController.addTodo
+    );
+    this.router.get(
+      `${this.baseURL}/todo`,
+      auth,
+      todoController.listTodos
+    );
+    this.router.delete(
+      `${this.baseURL}/todo/:todoId`,
+      auth,
+      authz,
+      todoController.deleteTodo
+    );
+    this.router.patch(
+      `${this.baseURL}/todo/:todoId/:status`,
+      auth,
+      authz,
+      todoController.patchStatusTodo
+    );
 
     return this.router;
   }
